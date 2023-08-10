dc761bf0b4acc71e15ac27ed9efe9bf10d980348
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-08-10 14:58:07 +0300

    add FunctionTests, add BasePoolInfoTests

---
+++ tests/MetaDataAPI.Tests/BasePoolInfoTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using System.Numerics;
+using FluentAssertions;
+
+namespace MetaDataAPI.Tests;
+
+public class BasePoolInfoTests
+{
+    [Fact]
+    public void Constructor_ShouldThrowInvalidOperationException_WhenParamsCountsMismatch()
+    {
+        var provider = new Provider("0000000000000000000000002028c98ac1702e2bb934a3e88734ccae42d44338");
+        var poolId = new BigInteger(0);
+        const string owner = "0x57e0433551460e85dfc5a5ddaff4db199d0f960a";
+        const string token = "0x66134461c865f824d294d8ca0d9080cc1acd05f6";
+        var parameters = new BigInteger[] { new(1), new(2) };
+
+        Action act = () => _ = new BasePoolInfo(provider, poolId, owner, token, parameters);
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("Mismatch between keys and params counts");
+    }
+}
