3bf0e5866be988d952a314a50e35fa37e513d24f
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-08-15 15:25:26 +0300

    fix FunctionTests

--- tests/MetaDataAPI.Tests/FunctionTests.cs
+++ tests/MetaDataAPI.Tests/FunctionTests.cs
@@ -1,56 +1,43 @@
-﻿//using Xunit;
-//using System.Net;
-//using System.Numerics;
-//using FluentAssertions;
-//using Flurl.Http.Testing;
-//using Newtonsoft.Json.Linq;
-//using MetaDataAPI.Tests.Helpers;
-//using MetaDataAPI.Models.Response;
-//using Amazon.Lambda.APIGatewayEvents;
+﻿using Xunit;
+using System.Net;
+using System.Numerics;
+using FluentAssertions;
+using Flurl.Http.Testing;
+using Newtonsoft.Json.Linq;
+using MetaDataAPI.Tests.Helpers;
+using MetaDataAPI.Models.Response;
+using MetaDataAPI.Providers.Simple;
+using Amazon.Lambda.APIGatewayEvents;
 
-//namespace MetaDataAPI.Tests;
+namespace MetaDataAPI.Tests;
 
-//public class FunctionTests : SetEnvironments
-//{
-//    private const string rpcUrl = "https://endpoints.omniatech.io/v1/bsc/testnet/public";
+public class FunctionTests : SetEnvironments
+{
+    [Fact]
+    public void FunctionHandler_ShouldReturnCorrectResponse()
+    {
+        var httpTest = new HttpTest();
+        httpTest
+            .ForCallsTo(HttpMock.RpcUrl)
+            .RespondWith(HttpMock.DealResponse);
 
-//    public FunctionTests()
-//    {
-//        Environment.SetEnvironmentVariable("RPC_URL", rpcUrl);
-//        Environment.SetEnvironmentVariable("LOCK_DEAL_NFT_ADDRESS", "0x57e0433551460e85dfC5a5DdafF4DB199D0F960A");
-//    }
+        var request = new APIGatewayProxyRequest
+        {
+            QueryStringParameters = new Dictionary<string, string> { { "id", "0" } }
+        };
 
-//    [Fact]
-//    public void FunctionHandler_ShouldReturnCorrectResponse()
-//    {
-        
-//        const string metadata = "0x0000000000000000000000002028c98ac1702e2bb934a3e88734ccae42d44338000000000000000000000000000000000000000000000000000000000000000000000000000000000000000057e0433551460e85dfc5a5ddaff4db199d0f960a00000000000000000000000066134461c865f824d294d8ca0d9080cc1acd05f600000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000";
-//        var rpcResponse = new JObject
-//        {
-//            { "result", metadata }
-//        };
-//        var httpTest = new HttpTest();
-//        httpTest
-//            .ForCallsTo(rpcUrl)
-//            .RespondWithJson(rpcResponse);
+        var response = new Function().FunctionHandler(request);
 
-//        var request = new APIGatewayProxyRequest
-//        {
-//            QueryStringParameters = new Dictionary<string, string> { { "id", "0" } }
-//        };
-
-//        var response = new Function().FunctionHandler(request);
-
-//        response.StatusCode.Should().Be((int)HttpStatusCode.OK);
-//        response.Headers.Should().Contain(new KeyValuePair<string, string>("Content-Type", "application/json"));
-//        response.Body.Should().BeEquivalentTo(JObject.FromObject(
-//            new Erc721Metadata(new BasePoolInfo(
-//                provider: new Provider("0000000000000000000000002028c98ac1702e2bb934a3e88734ccae42d44338"),
-//                poolId: new BigInteger(0),
-//                owner: "0x57e0433551460e85dfc5a5ddaff4db199d0f960a",
-//                token: "0x66134461c865f824d294d8ca0d9080cc1acd05f6",
-//                new BigInteger[] { new(0) }
-//            ))
-//        ).ToString());
-//    }
-//}
+        response.StatusCode.Should().Be((int)HttpStatusCode.OK);
+        response.Headers.Should().Contain(new KeyValuePair<string, string>("Content-Type", "application/json"));
+        response.Body.Should().BeEquivalentTo(JObject.FromObject(
+            new Erc721Metadata(new BasePoolInfo(
+                provider: new DealProvider(),
+                poolId: new BigInteger(0),
+                owner: "0x57e0433551460e85dfc5a5ddaff4db199d0f960a",
+                token: "0x66134461c865f824d294d8ca0d9080cc1acd05f6",
+                new BigInteger[] { new(0) }
+            ))
+        ).ToString());
+    }
+}
