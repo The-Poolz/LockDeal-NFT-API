f8ae7540af5589e13ff46eadb1c98274f61cdb20
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-08-17 12:40:08 +0300

    add DisplayType and DisplayTypeExtensions

---
+++ src/MetaDataAPI/Models/Types/DisplayTypeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace MetaDataAPI.Models.Types;
+
+public static class DisplayTypeExtensions
+{
+    public static string ToLowerString(this DisplayType displayType) =>
+        displayType.ToString().ToLower();
+}
