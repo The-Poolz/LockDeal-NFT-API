947f8186824fa0c197c722c66712611d47138fb9
Roy Kodman <roykod18@gmail.com>
2023-09-01 18:20:37 +0300

    - update `IProvider` - refactor `DealProvider`

--- src/MetaDataAPI/Providers/IProvider.cs
+++ src/MetaDataAPI/Providers/IProvider.cs
@@ -1,10 +1,9 @@
-﻿using System.Numerics;
-using MetaDataAPI.Models.Response;
+﻿using MetaDataAPI.Models.Response;
 
 namespace MetaDataAPI.Providers;
 
 public interface IProvider
 {
     public byte ParametersCount { get; }
-    public IEnumerable<Erc721Attribute> GetAttributes(params BigInteger[] values);
+    public List<Erc721Attribute> Attributes { get; }
 }
