ef1b137d50919364c9b327530349d8f10d9351ad
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-05 12:39:35 +0300

    implement `GetDescription` in `BundleProvider`

--- src/MetaDataAPI/Providers/Advanced/BundleProvider.cs
+++ src/MetaDataAPI/Providers/Advanced/BundleProvider.cs
@@ -1,17 +1,25 @@
-﻿using System.Numerics;
+﻿using System.Text;
+using System.Numerics;
+using MetaDataAPI.Utils;
 using MetaDataAPI.Models.Response;
 
 namespace MetaDataAPI.Providers.Advanced;
 
 public class BundleProvider : IProvider
 {
+    private readonly BigInteger poolId;
+    private readonly BigInteger lastSubPoolId;
+    private readonly byte decimals;
     public byte ParametersCount => 1;
     public List<Erc721Attribute> Attributes { get; }
 
     public BundleProvider(BigInteger poolId, byte decimals, IReadOnlyList<BigInteger> values)
     {
+        this.poolId = poolId;
+        lastSubPoolId = values[0];
+        this.decimals = decimals;
         Attributes = new List<Erc721Attribute>();
-        for (var id = poolId + 1; id <= values[0]; id++)
+        for (var id = poolId + 1; id <= lastSubPoolId; id++)
         {
             var providerAttributes = AttributesService.GetProviderAttributes(id, decimals);
 
@@ -22,6 +30,18 @@ public class BundleProvider : IProvider
 
     public string GetDescription(string token)
     {
-        throw new NotImplementedException();
+        var descriptionBuilder = new StringBuilder("This NFT orchestrates a series of sub-pools to enable sophisticated asset management strategies. The following are the inner pools under its governance:");
+
+        for (var id = poolId + 1; id <= lastSubPoolId; id++)
+        {
+            var metadata = RpcCaller.GetMetadata(poolId);
+            var parser = new MetadataParser(metadata);
+
+            var provider = ProviderFactory.Create(parser.GetProviderAddress(), poolId, decimals, parser.GetProviderParameters().ToArray());
+
+            descriptionBuilder.AppendLine($"- {id}: {provider.GetDescription(token)}");
+        }
+
+        return descriptionBuilder.ToString();
     }
 }
