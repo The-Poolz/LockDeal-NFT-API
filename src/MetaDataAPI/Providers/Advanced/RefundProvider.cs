75a15fbb759af5808d39e0b16d96cd499efe0ac7
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-05 12:21:31 +0300

    implement `GetDescription` in `RefundProvider`

--- src/MetaDataAPI/Providers/Advanced/RefundProvider.cs
+++ src/MetaDataAPI/Providers/Advanced/RefundProvider.cs
@@ -7,11 +7,15 @@ namespace MetaDataAPI.Providers.Advanced;
 
 public class RefundProvider : IProvider
 {
+    private readonly BigInteger poolId;
+    private readonly byte decimals;
     public byte ParametersCount => 2;
     public List<Erc721Attribute> Attributes { get; }
 
     public RefundProvider(BigInteger poolId, byte decimals, IReadOnlyList<BigInteger> values)
     {
+        this.poolId = poolId;
+        this.decimals = decimals;
         Attributes = new List<Erc721Attribute>
         {
             new("Rate", new ConvertWei(18).WeiToEth(values[1]), DisplayType.Number),
@@ -23,6 +27,10 @@ public class RefundProvider : IProvider
 
     public string GetDescription(string token)
     {
-        throw new NotImplementedException();
+        var dealProviderAttributes = AttributesService.GetProviderAttributes(poolId + 3, decimals).ToArray();
+        var mainCoin = Attributes[1].Value;
+        var mainCoinAmountCalc = Attributes[3].Value; // not sure
+
+        return $"This NFT encompasses {dealProviderAttributes[0].Value} units of the asset {token} with an associated refund rate of {Attributes[1].Value}. Post rate calculation, the refundable amount in the primary asset {mainCoin} will be {mainCoinAmountCalc}.";
     }
 }
