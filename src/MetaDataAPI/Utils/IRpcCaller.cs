3277f06bfc417b0638226832634ebc9b8b46b95b
Stan Goldin <salty87@gmail.com>
2023-09-08 01:02:26 +0300

    fix smell

--- src/MetaDataAPI/Utils/IRpcCaller.cs
+++ src/MetaDataAPI/Utils/IRpcCaller.cs
@@ -5,7 +5,7 @@ namespace MetaDataAPI.Utils
     public interface IRpcCaller
     {
         public string GetMetadata(BigInteger poolId);
-        public string GetName(string providerAddress);
+        public string GetName(string address);
         public string GetSymbol(string address);
         public byte GetDecimals(string token);
 
