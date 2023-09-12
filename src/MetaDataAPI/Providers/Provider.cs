3ef5b327d9ed1f775e9ac9fdad937a0cfc5f5493
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2023-09-12 18:13:02 +0300

    add abstract (#91)
    * add abstract
    
    * AddAttributes("TimedDealProvider");
    
    * fix empty line

---
+++ src/MetaDataAPI/Providers/Provider.cs
@@ -0,0 +1,23 @@
+﻿using MetaDataAPI.Models.Response;
+
+namespace MetaDataAPI.Providers;
+
+public abstract class Provider : IProvider
+{
+    protected Provider(BasePoolInfo basePoolInfo)
+    {
+        PoolInfo = basePoolInfo;
+        Attributes = new();
+    }
+    public BasePoolInfo PoolInfo { get;}
+
+    public List<Erc721Attribute> Attributes { get; }
+
+    public abstract string GetDescription();
+    internal void AddAttributes(string name)
+    {
+        Attributes.Add(new Erc721Attribute("ProviderName", name, Models.Types.DisplayType.String));
+        Attributes.AddRange(GetParams());
+    }
+    public abstract List<Erc721Attribute> GetParams();
+}
--- src/MetaDataAPI/Providers/Simple/LockDealProvider.cs
+++ src/MetaDataAPI/Providers/Simple/LockDealProvider.cs
@@ -4,25 +4,22 @@ using MetaDataAPI.Models.Response;
 
 namespace MetaDataAPI.Providers;
 
-public class LockDealProvider : IProvider
+public class LockDealProvider : Provider
 {
-    public List<Erc721Attribute> Attributes => new()
-    {
-        new Erc721Attribute("LeftAmount", LeftAmount, DisplayType.Number),
-        new Erc721Attribute("StartTime", StartTime, DisplayType.Date)
-    };
-    public BasePoolInfo PoolInfo { get; }
     public decimal LeftAmount { get; }
     public uint StartTime { get; }
-
-    public LockDealProvider(BasePoolInfo basePoolInfo)
+    public LockDealProvider(BasePoolInfo basePoolInfo) : base(basePoolInfo)
     {
-        PoolInfo = basePoolInfo;
         var converter = new ConvertWei(basePoolInfo.Token.Decimals);
         LeftAmount = converter.WeiToEth(basePoolInfo.Params[0]);
         StartTime = (uint)new ConvertWei(0).WeiToEth(basePoolInfo.Params[1]);
+        AddAttributes("LockDealProvider");
     }
-
-    public string GetDescription() =>
+    public override string GetDescription() =>
         $"This NFT securely locks {LeftAmount} units of the asset {PoolInfo.Token}. Access to these assets will commence on the designated start time of {TimeUtils.FromUnixTimestamp(StartTime)}.";
+    public override List<Erc721Attribute> GetParams() => new()
+    {
+            new Erc721Attribute("LeftAmount", LeftAmount, DisplayType.Number),
+            new Erc721Attribute("StartTime", StartTime, DisplayType.Date)
+    };
 }
