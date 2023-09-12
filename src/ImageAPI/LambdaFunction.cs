3446039cd79e7b72c4e03347e8a1de861837d571
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-09-12 14:56:31 +0300

    Rename `Function.cs` to `LambdaFunction.cs` (#82)
    * Rename `Function.cs` to `LambdaFunction.cs`
    
    * update dependencies (#85)

--- src/ImageAPI/LambdaFunction.cs
+++ src/ImageAPI/LambdaFunction.cs
@@ -6,12 +6,11 @@ using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using System.Reflection;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace ImageAPI;
 
-public class Function
+public class LambdaFunction
 {
     public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
