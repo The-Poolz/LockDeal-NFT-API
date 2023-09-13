401f237286a645f5fa48ee8c63ac21aafce6f941
Roy Kodman <roykod18@gmail.com>
2023-09-13 11:31:13 +0300

    - continue tests

---
+++ tests/ImageAPI.Test/Utils/ImageProcessorTests.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using Xunit;
+
+namespace ImageAPI.Test.Utils;
+
+public class ImageProcessorTests
+{
+}
