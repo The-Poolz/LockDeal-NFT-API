56b64c154a73f960f70ac22e93ebb0735376ba1d
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-14 18:57:05 +0300

    refactor ErrorMessages

--- src/MetaDataAPI/Storage/ErrorMessages.cs
+++ src/MetaDataAPI/Storage/ErrorMessages.cs
@@ -2,9 +2,9 @@
 
 public static class ErrorMessages
 {
-    public const string missingIdMessage = "The 'id' parameter is missing.";
-    public const string invalidIdMessage = "The 'id' parameter is not a valid BigInteger.";
-    public const string invalidResponseMessage = "Id from metadata needs to be the same as Id from request.";
-    public const string failedToCreateProviderMessage = "Failed to create provider.";
-    public const string poolIdNotInRangeMessage = "The 'id' need to be less then total supply";
+    public const string MissingIdMessage = "The 'id' parameter is missing.";
+    public const string InvalidIdMessage = "The 'id' parameter is not a valid BigInteger.";
+    public const string InvalidResponseMessage = "Id from metadata needs to be the same as Id from request.";
+    public const string FailedToCreateProviderMessage = "Failed to create provider.";
+    public const string PoolIdNotInRangeMessage = "The 'id' need to be less then total supply";
 }
