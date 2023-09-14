2bc34b4cdf4410e79019686ff45229b9d174185f
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-09-14 13:46:31 +0300

    Split `FunctionHandler` by more functions (#86)
    * Rename `Function.cs` to `LambdaFunction.cs`
    
    * split `FunctionHandler` by more functions
    
    * split by classes
    
    * Write tests for `ImageAPI` (#88)
    
    * move ResponseBuilder
    
    * add Moq library
    
    * - create tests for `ResourcesLoader`
    
    * - continue tests
    
    * start ImageProcessorTests
    
    * - tests for `ImageProcessor`
    
    * - make `ResourcesLoaderTests` better
    
    * - finish writing `ImageAPI` tests
    
    * - rename FunctionHandler to RunAsync
    - write first test for LambdaFunction
    
    * add more test
    
    * cleanup tests
    
    ---------
    
    Co-authored-by: Roy Kodman <roykod18@gmail.com>
    
    * cleanup ImageProcessorTests
    
    * - make ResourcesLoader non-static
    - write two more tests for ResourcesLoader
    
    * remove TODO
    
    ---------
    
    Co-authored-by: Roy Kodman <roykod18@gmail.com>

---
+++ tests/ImageAPI.Test/FunctionHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using ImageAPI.Utils;
+using FluentAssertions;
+using Amazon.Lambda.APIGatewayEvents;
+using Moq;
+
+namespace ImageAPI.Test;
+
+public class FunctionHandlerTests
+{
+    [Fact]
+    internal async Task FunctionHandler_ShouldReturnResponse_WrongInput()
+    {
+        var request = new APIGatewayProxyRequest
+        {
+            QueryStringParameters = new Dictionary<string, string>
+            {
+                { "id", "not number" }
+            }
+        };
+
+        var result = await new LambdaFunction().RunAsync(request);
+
+        result.Should().BeEquivalentTo(ResponseBuilder.WrongInput());
+    }
+
+    [Fact]
+    internal async Task FunctionHandler_ShouldReturnResponse_ImageResponse()
+    {
+        var expected = ResponseBuilder.ImageResponse("base64ImageHere");
+        var request = new APIGatewayProxyRequest
+        {
+            QueryStringParameters = new Dictionary<string, string>
+            {
+                { "id", "1" }
+            }
+        };
+
+        var result = await new LambdaFunction().RunAsync(request);
+
+        result.Body.Should().NotBe(string.Empty);
+        result.Headers.Should().BeEquivalentTo(expected.Headers);
+        result.IsBase64Encoded.Should().Be(expected.IsBase64Encoded);
+        result.StatusCode.Should().Be(expected.StatusCode);
+    }
+
+    [Fact]
+    internal async Task FunctionHandler_ShouldReturnResponse_GeneralError()
+    {
+        var imageProcessor = new Mock<ImageProcessor>();
+        imageProcessor
+            .Setup(x => x.CreateTextOptions(It.IsAny<float>(), It.IsAny<float>(), It.IsAny<float>()))
+            .Throws<InvalidOperationException>();
+
+        var request = new APIGatewayProxyRequest
+        {
+            QueryStringParameters = new Dictionary<string, string>
+            {
+                { "id", "1" }
+            }
+        };
+
+        var result = await new LambdaFunction(imageProcessor.Object).RunAsync(request);
+
+        result.Should().BeEquivalentTo(ResponseBuilder.GeneralError());
+    }
+}
