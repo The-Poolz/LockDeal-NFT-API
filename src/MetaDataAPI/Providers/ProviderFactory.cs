91a6a4c96cb836570150df105357176e51281dde
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-09-14 18:29:09 +0300

    refactor ProviderFactory, add one ProviderFactoryTests

--- src/MetaDataAPI/Providers/ProviderFactory.cs
+++ src/MetaDataAPI/Providers/ProviderFactory.cs
@@ -13,16 +13,28 @@ public class ProviderFactory
     {
         this.rpcCaller = rpcCaller ?? new RpcCaller();
     }
+
     public Erc20Token GetErc20Token(string address) => new(address, rpcCaller);
+
     public bool IsPoolIdWithinSupplyRange(BigInteger poolId) =>
         rpcCaller.GetTotalSupply(Environments.LockDealNftAddress) > poolId;
-    public Provider Create(BigInteger poolId) => Create(rpcCaller.GetMetadata(poolId));
-    private Provider Create(string metadata) => Create(new BasePoolInfo(metadata,this));
-    private Provider Create(BasePoolInfo basePoolInfo)
-    {    
-        var name = rpcCaller.GetName(basePoolInfo.ProviderAddress);
-        var objectToInstantiate = $"MetaDataAPI.Providers.{name}, MetaDataAPI";
-        var objectType = Type.GetType(objectToInstantiate);
-        return (Provider)Activator.CreateInstance(objectType!, args: basePoolInfo)!;
-    }
+
+    public Provider Create(BigInteger poolId) =>
+        Create(rpcCaller.GetMetadata(poolId));
+
+    private Provider Create(string metadata) =>
+        Create(new BasePoolInfo(metadata,this));
+
+    private static Provider Create(BasePoolInfo basePoolInfo) =>
+        Providers(basePoolInfo)[basePoolInfo.ProviderAddress]();
+
+    public static Dictionary<string, Func<Provider>> Providers(BasePoolInfo basePoolInfo) => new()
+    {
+        { "0x6B31bE09cF4e2da92F130B1056717fEa06176CeD".ToLower(), () => new DealProvider(basePoolInfo) },
+        { "0xB9FD557C192939a3889080954D52c64eBA8E9Be3".ToLower(), () => new LockDealProvider(basePoolInfo) },
+        { "0x724A076a45ee73544685d4A9Fc2240B1C635711e".ToLower(), () => new TimedDealProvider(basePoolInfo) },
+        { "0x70dECfD5e51C59EBdC8AcA96bf22da6aFF00b176".ToLower(), () => new BundleProvider(basePoolInfo) },
+        { "0x7254A337D05d3965D7D3d8c1a94Cd1CFCD1b00d6".ToLower(), () => new RefundProvider(basePoolInfo) },
+        { "0x8Bf8Cf18c5cB5De075978394624674bA19b96d1B".ToLower(), () => new CollateralProvider(basePoolInfo) }
+    };
 }
---
+++ tests/MetaDataAPI.Tests/Providers/ProviderFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using System.Reflection;
+using MetaDataAPI.Providers;
+using MetaDataAPI.Tests.Helpers;
+using MetaDataAPI.Models.Response;
+
+namespace MetaDataAPI.Tests.Providers;
+
+public class ProviderFactoryTests
+{
+    [Fact]
+    internal void Providers_NumberOfInheritedClassesBeEqual()
+    {
+        var assembly = Assembly.Load("MetaDataAPI");
+        var derivedTypes = assembly.GetTypes()
+            .Where(t => t is { IsClass: true, IsAbstract: false } && t.IsSubclassOf(typeof(Provider)))
+            .ToList();
+
+        var countOfImplemented = ProviderFactory.Providers(new BasePoolInfo(StaticResults.MetaData[0], new ProviderFactory(new MockRpcCaller()))).ToArray().Length;
+
+        Assert.Equal(countOfImplemented, derivedTypes.Count);
+    }
+}
