d4607fed2b92daac0aa53865c23c0dcd5578d88d
Roy Kodman <roykod18@gmail.com>
2023-10-05 19:29:25 +0300

    Revert "Small correction"
    This reverts commit fea56a12bc267a2c35a3477ad2dcb29091ddace4.

--- src/ImageAPI/LambdaFunction.cs
+++ src/ImageAPI/LambdaFunction.cs
@@ -66,7 +66,7 @@ public class LambdaFunction
         {
             var providerImage = ProviderImageFactory.Create(backgroundImage, font, attributes);
 
-            dynamoDb.UpdateItemAsync(hash, providerImage.Base64Image, providerImage.ContentType)
+            dynamoDb.UpdateItemAsync(hash, providerImage.Base64Image, databaseItem.Item["Content-Type"].S)
                 .GetAwaiter()
                 .GetResult();
 
