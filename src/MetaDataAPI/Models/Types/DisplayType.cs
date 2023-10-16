2c13eac14c67f2f38e20804ecd871c123f53651e
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-10-16 13:58:42 +0300

    Fix `DisplayType` (#148)
    * MetaDataAPI: save attributes into DynamoDb, change image URL
    
    * Fix DisplayType

--- src/MetaDataAPI/Models/Types/DisplayType.cs
+++ src/MetaDataAPI/Models/Types/DisplayType.cs
@@ -1,9 +1,17 @@
-﻿namespace MetaDataAPI.Models.Types;
+﻿using System.Runtime.Serialization;
+
+namespace MetaDataAPI.Models.Types;
 
 public enum DisplayType
 {
+    [EnumMember(Value = "none")]
+    None,
+    [EnumMember(Value = "string")]
     String,
+    [EnumMember(Value = "number")]
     Number,
+    [EnumMember(Value = "date")]
     Date,
+    [EnumMember(Value = "ignore")]
     Ignore
 }
