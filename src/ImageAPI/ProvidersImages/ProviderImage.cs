6ccb151a7a35fdd0ea972756283fd485c8309b03
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-11-01 18:12:28 +0300

    - update `ProviderImage` - update `GifCreator`

--- src/ImageAPI/ProvidersImages/ProviderImage.cs
+++ src/ImageAPI/ProvidersImages/ProviderImage.cs
@@ -21,7 +21,6 @@ public abstract class ProviderImage
         get
         {
             using var outputStream = new MemoryStream();
-
             if (ContentType == "image/png")
             {
                 Image.SaveAsPngAsync(outputStream)
@@ -35,7 +34,6 @@ public abstract class ProviderImage
                     .GetResult();
             }
             var imageBytes = outputStream.ToArray();
-
             return Convert.ToBase64String(imageBytes);
         }
     }
@@ -81,7 +79,7 @@ public abstract class ProviderImage
         var gifMetaData = gif.Metadata.GetGifMetadata();
         gifMetaData.RepeatCount = 0;
 
-        SlideImages(gif, images, frameDelay);
+        GifCreator.SlideImages(gif, images, frameDelay);
 
         Image = gif.Clone(_ => { });
         ContentType = "image/gif";
@@ -94,47 +92,5 @@ public abstract class ProviderImage
             return coordinates;
         }
         return null;
-    }
-
-    private static void SlideImages(Image gif, IReadOnlyList<Image> images, int frameDelay)
-    {
-        const int slideSteps = 20;
-        var stepSize = images[0].Width / slideSteps;
-
-        const int staticFrameDelay = 500;
-
-        for (var i = 0; i < images.Count; i++)
-        {
-            var currentImage = images[i];
-            var nextImage = images[(i + 1) % images.Count];
-
-            using (var staticFrame = new Image<Rgba32>(currentImage.Width, currentImage.Height))
-            {
-                staticFrame.Mutate(ctx => ctx.DrawImage(currentImage, new Point(0, 0), 1f));
-                var staticMetadata = staticFrame.Frames.RootFrame.Metadata.GetGifMetadata();
-                staticMetadata.FrameDelay = staticFrameDelay;
-                gif.Frames.AddFrame(staticFrame.Frames.RootFrame);
-            }
-
-            for (var step = 0; step < slideSteps; step++)
-            {
-                var offset = step * stepSize;
-                using var frame = new Image<Rgba32>(currentImage.Width, currentImage.Height);
-
-                if (-offset < frame.Width && currentImage.Width - offset > 0)
-                {
-                    frame.Mutate(ctx => ctx.DrawImage(currentImage, new Point(-offset, 0), 1f));
-                }
-
-                if (currentImage.Width - offset < frame.Width && offset > 0)
-                {
-                    frame.Mutate(ctx => ctx.DrawImage(nextImage, new Point(currentImage.Width - offset, 0), 1f));
-                }
-
-                var metadata = frame.Frames.RootFrame.Metadata.GetGifMetadata();
-                metadata.FrameDelay = frameDelay;
-                gif.Frames.AddFrame(frame.Frames.RootFrame);
-            }
-        }
     }
 }
--- src/ImageAPI/Utils/GifCreator.cs
+++ src/ImageAPI/Utils/GifCreator.cs
@@ -1,6 +1,50 @@
-﻿namespace ImageAPI.Utils;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
 
-public class GifCreator
+namespace ImageAPI.Utils;
+
+public static class GifCreator
 {
+    public static void SlideImages(Image gif, IReadOnlyList<Image> images, int frameDelay)
+    {
+        const int slideSteps = 20;
+        var stepSize = images[0].Width / slideSteps;
 
+        const int staticFrameDelay = 500;
+
+        for (var i = 0; i < images.Count; i++)
+        {
+            var currentImage = images[i];
+            var nextImage = images[(i + 1) % images.Count];
+
+            using (var staticFrame = new Image<Rgba32>(currentImage.Width, currentImage.Height))
+            {
+                staticFrame.Mutate(ctx => ctx.DrawImage(currentImage, new Point(0, 0), 1f));
+                var staticMetadata = staticFrame.Frames.RootFrame.Metadata.GetGifMetadata();
+                staticMetadata.FrameDelay = staticFrameDelay;
+                gif.Frames.AddFrame(staticFrame.Frames.RootFrame);
+            }
+
+            for (var step = 0; step < slideSteps; step++)
+            {
+                var offset = step * stepSize;
+                using var frame = new Image<Rgba32>(currentImage.Width, currentImage.Height);
+
+                if (-offset < frame.Width && currentImage.Width - offset > 0)
+                {
+                    frame.Mutate(ctx => ctx.DrawImage(currentImage, new Point(-offset, 0), 1f));
+                }
+
+                if (currentImage.Width - offset < frame.Width && offset > 0)
+                {
+                    frame.Mutate(ctx => ctx.DrawImage(nextImage, new Point(currentImage.Width - offset, 0), 1f));
+                }
+
+                var metadata = frame.Frames.RootFrame.Metadata.GetGifMetadata();
+                metadata.FrameDelay = frameDelay;
+                gif.Frames.AddFrame(frame.Frames.RootFrame);
+            }
+        }
+    }
 }
