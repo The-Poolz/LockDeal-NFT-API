ecd68cbcf25dfc4c0269e02bfd39f8c8e59ebc21
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-11 14:11:10 +0300

    - update `LockDealProviderImage`

--- src/ImageAPI/LambdaFunction.cs
+++ src/ImageAPI/LambdaFunction.cs
@@ -61,7 +61,7 @@ public class LambdaFunction
 
         try
         {
-            var attributes = JsonConvert.DeserializeObject<DynamoDbItem[]>("[{\"ProviderName\":\"DealProvider\",\"Attributes\":[{\"trait_type\":\"Collection\",\"value\":2},{\"trait_type\":\"LeftAmount\",\"value\":13572.37461}]}]")!;
+            var attributes = JsonConvert.DeserializeObject<DynamoDbItem[]>("[{\"ProviderName\":\"LockDealProvider\",\"Attributes\":[{\"display_type\":\"date\",\"trait_type\":\"StartTime\",\"value\":1699967146},{\"trait_type\":\"Collection\",\"value\":0},{\"trait_type\":\"LeftAmount\",\"value\":10000.0}]}]")!;
             var providerImage = ProviderImageFactory.Create(backgroundImage, font, attributes);
 
             //await dynamoDb.UpdateItemAsync(hash, providerImage.Base64Image);
--- src/ImageAPI/ProvidersImages/Simple/LockDealProviderImage.cs
+++ src/ImageAPI/ProvidersImages/Simple/LockDealProviderImage.cs
@@ -9,12 +9,16 @@ public class LockDealProviderImage : ProviderImage
 {
     public override IDictionary<string, PointF> AttributeCoordinates => new Dictionary<string, PointF>
     {
-        { "LeftAmount", new PointF(BackgroundImage.Width / 2f, BackgroundImage.Height / 2f) },
-        { "StartTime", new PointF(BackgroundImage.Width / 2f, BackgroundImage.Height / 3f) },
-        { "Collection", new PointF(BackgroundImage.Width / 2f, BackgroundImage.Height / 4f) }
+        { "LeftAmount", new PointF(BackgroundImage.Width - 400, BackgroundImage.Height - 290) },
+        { "StartTime", new PointF(BackgroundImage.Width - 1030, BackgroundImage.Height - 290) },
+        //{ "Collection", new PointF(BackgroundImage.Width / 2f, BackgroundImage.Height / 4f) }
     };
 
-    public override IDictionary<string, PointF> TextCoordinates { get; }
+    public override IDictionary<string, PointF> TextCoordinates => new Dictionary<string, PointF>
+    {
+        { "Left", new PointF(BackgroundImage.Width - 400, BackgroundImage.Height - 330) },
+        { "Start", new PointF(BackgroundImage.Width - 1030, BackgroundImage.Height - 330) }
+    };
 
     public LockDealProviderImage(Image backgroundImage, Font font, IReadOnlyList<DynamoDbItem> dynamoDbItems)
         : base(nameof(LockDealProvider), backgroundImage, font, dynamoDbItems[0])
