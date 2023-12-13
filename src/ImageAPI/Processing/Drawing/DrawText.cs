fefd77b756a05fdb5590f05401c070380396b787
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-13 18:23:59 +0300

    - refactor `ToDrawing` - refactor each `ToDrawing` implementation

--- src/ImageAPI/Processing/Drawing/DrawText.cs
+++ src/ImageAPI/Processing/Drawing/DrawText.cs
@@ -1,20 +1,14 @@
-﻿using ImageAPI.Utils;
-using SixLabors.Fonts;
-using SixLabors.ImageSharp;
+﻿using SixLabors.ImageSharp;
 
 namespace ImageAPI.Processing.Drawing;
 
 public class DrawText : ToDrawing
 {
-    public override string Text { get; }
-    public override Font Font { get; }
-    public override PointF Coordinates { get; }
-
     public DrawText(string text, float x, float y)
-    {
-        Text = text;
-        // TODO: Create class which provide caching fonts by fontSize. This class get exist font or create new and save it.
-        Font = new ResourcesLoader().LoadFontFromEmbeddedResources(TextFontSize);
-        Coordinates = new PointF(x, y);
-    }
+        : base(
+            text: text,
+            coordinates: new PointF(x, y),
+            fontSize: TextFontSize
+        )
+    { }
 }
