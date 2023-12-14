90f3ccbd760e9742f9cdbeedf84d477ef2915f80
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-14 14:15:24 +0300

    ImageAPI: Update printing DateTime data

--- src/ImageAPI/Processing/Drawing/DrawStartTime.cs
+++ src/ImageAPI/Processing/Drawing/DrawStartTime.cs
@@ -6,11 +6,16 @@ namespace ImageAPI.Processing.Drawing;
 
 public class DrawStartTime : ToDrawing
 {
+    private readonly DateTime startTime;
+
     public DrawStartTime(object startTime)
-        : base(
-            text: TimeUtils.FromUnixTimestamp((long)startTime).ToString(CultureInfo.InvariantCulture),
-            coordinates: new PointF(ImageSize.Width - 1030, ImageSize.Height - 290),
-            fontSize: DateTimeFontSize
-        )
-    { }
+    {
+        this.startTime = TimeUtils.FromUnixTimestamp((long)startTime);
+    }
+
+    public override Image Draw(Image drawOn)
+    {
+        drawOn = Draw(drawOn, startTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture), new PointF(ImageSize.Width - 1030, ImageSize.Height - 290), DateTimeFontSize);
+        return Draw(drawOn, startTime.ToString("HH:mm:ss", CultureInfo.InvariantCulture), new PointF(ImageSize.Width - 1030, ImageSize.Height - 270), DateTimeFontSize);
+    }
 }
