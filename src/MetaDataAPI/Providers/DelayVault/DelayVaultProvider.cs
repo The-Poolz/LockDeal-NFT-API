db830f1a522721d5e903443f4e9ba9885bf2ee58
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-12-19 14:50:47 +0300

    MetaDataAPI: Create `DelayVaultProvider` (#197)
    * - add DelayVaultProvider.cs
    
    * - rename
    
    * - update description
    - add tests for DelayVaultProvider

---
+++ src/MetaDataAPI/Providers/DelayVault/DelayVaultProvider.cs
@@ -0,0 +1,23 @@
+﻿using MetaDataAPI.Models.DynamoDb;
+using MetaDataAPI.Models.Response;
+
+namespace MetaDataAPI.Providers;
+
+public class DelayVaultProvider : Provider
+{
+    public override string ProviderName => nameof(DelayVaultProvider);
+    public override string Description => $"The DelayVaultProvider manages the locking of {LeftAmount} tokens {PoolInfo.Token} for leaderboard purposes. While tokens are locked, users accumulate leaderboard points.";
+
+    public override List<DynamoDbItem> DynamoDbAttributes => new()
+    {
+        new DynamoDbItem(ProviderName, PoolInfo.PoolId, new List<Erc721Attribute>
+        {
+            new("Collection", Collection),
+            new("LeftAmount", LeftAmount)
+        })
+    };
+
+    public DelayVaultProvider(BasePoolInfo basePoolInfo)
+        : base(basePoolInfo)
+    { }
+}
