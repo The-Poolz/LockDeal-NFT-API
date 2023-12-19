e50843f1c81e403155703cd8e75273ab24a53906
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-19 15:48:21 +0300

    MetaDataAPI: New one environment variable: `NAME_OF_STAGE`

--- src/MetaDataAPI/Storage/Environments.cs
+++ src/MetaDataAPI/Storage/Environments.cs
@@ -7,4 +7,5 @@ public static class Environments
     private static readonly EnvManager envManager = new();
     public static string LockDealNftAddress => envManager.GetEnvironmentValue<string>("LOCK_DEAL_NFT_ADDRESS", true);
     public static string RpcUrl => envManager.GetEnvironmentValue<string>("RPC_URL", true);
+    public static string NameOfStage => envManager.GetEnvironmentValue<string>("NAME_OF_STAGE", true);
 }
