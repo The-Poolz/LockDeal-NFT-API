7f4ea49fddba2f9120f961c09856c7bf682a7d74
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-21 15:06:38 +0300

    - create `TruncateFractionalPart` method

--- src/ImageAPI/Processing/Drawing/DrawLeftAmount.cs
+++ src/ImageAPI/Processing/Drawing/DrawLeftAmount.cs
@@ -10,12 +10,22 @@ public class DrawLeftAmount : ToDrawing
 
     public DrawLeftAmount(object leftAmount)
     {
-        this.leftAmount = leftAmount.ToString()!;
+        this.leftAmount = TruncateFractionalPart(leftAmount.ToString()!);
     }
 
     public override Image Draw(Image drawOn)
     {
         drawOn = Draw(drawOn, new ToDrawParameters("Left Amount", LeftAmount.HeaderPosition, HeaderFontSize));
         return Draw(drawOn, new ToDrawParameters(leftAmount, LeftAmount.Position, LeftAmount.FontSize, 5.5f, FontStyle.Bold));
+    }
+
+    private static string TruncateFractionalPart(string number)
+    {
+        var dotIndex = number.IndexOf('.');
+        if (dotIndex != -1 && number.Length > dotIndex + 3)
+        {
+            return number[..(dotIndex + 3)] + "...";
+        }
+        return number;
     }
 }
