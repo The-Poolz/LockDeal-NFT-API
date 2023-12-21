bbdd36b9b151a747f3961b1012a357a831164641
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-21 12:53:40 +0300

    - remove `ImageExtensions` - cleanup

--- src/ImageAPI/Processing/ImageProcessor.cs
+++ src/ImageAPI/Processing/ImageProcessor.cs
@@ -3,7 +3,6 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.PixelFormats;
 
 namespace ImageAPI.Processing;
 
