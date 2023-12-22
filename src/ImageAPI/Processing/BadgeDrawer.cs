ff5321ab3ed841c78b6f51c369e61615212b7169
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-12-22 14:09:08 +0300

    - split `ImageExtensions`

--- src/ImageAPI/Processing/BadgeDrawer.cs
+++ src/ImageAPI/Processing/BadgeDrawer.cs
@@ -1,80 +1,14 @@
-﻿using System.Numerics;
-using SixLabors.Fonts;
+﻿using SixLabors.Fonts;
 using ImageAPI.Settings;
-using MetaDataAPI.Utils;
 using SixLabors.ImageSharp;
-using System.Globalization;
 using SixLabors.ImageSharp.Drawing;
-using System.Text.RegularExpressions;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Drawing.Processing;
 
 namespace ImageAPI.Processing;
 
-public static class ImageExtensions
+public static class BadgeDrawer
 {
-    public static void DrawText(this Image drawOn, string text, PointF coordinates, float fontSize, FontStyle fontStyle = FontStyle.Regular, float penWidth = 2f, Color color = default)
-    {
-        color = color == default ? ColorPalette.White : color;
-        var textOptions = new RichTextOptions(Resources.Font(fontSize, fontStyle))
-        {
-            HorizontalAlignment = HorizontalAlignment.Left,
-            Origin = new PointF(coordinates.X - penWidth, coordinates.Y - penWidth)
-        };
-
-        drawOn.Mutate(x => x.DrawText(
-            textOptions,
-            text,
-            Brushes.Solid(color), 
-            Pens.Solid(color, penWidth)
-        ));
-    }
-
-    public static void DrawHeader(this Image drawOn, string text, PointF coordinates)
-    {
-        drawOn.DrawText(text, coordinates, 16f);
-    }
-
-    public static void DrawProviderName(this Image drawOn, string providerName)
-    {
-        drawOn.DrawText(Regex.Replace(providerName, "(?<!^)([A-Z])", " $1"), new PointF(672, 52), 24f);
-    }
-
-    public static void DrawLeftAmount(this Image drawOn, object leftAmount)
-    {
-        drawOn.DrawHeader("Left Amount", new PointF(672, 274));
-        drawOn.DrawText(leftAmount.ToString()!, new PointF(672, 298), 64f, FontStyle.Bold, 5.5f);
-    }
-
-    public static void DrawStartTime(this Image drawOn, object startTime)
-    {
-        drawOn.DrawHeader("Start Time", new PointF(48, 274));
-        drawOn.DrawDate(startTime, new PointF(48, 298), 32f);
-        drawOn.DrawTime(startTime, new PointF(48, 340), 32f);
-    }
-
-    public static void DrawFinishTime(this Image drawOn, object finishTime)
-    {
-        drawOn.DrawHeader("Finish Time", new PointF(276, 274));
-        drawOn.DrawDate(finishTime, new PointF(276, 298), 32f);
-        drawOn.DrawTime(finishTime, new PointF(276, 340), 32f);
-    }
-
-    public static void DrawDate(this Image drawOn, object dateTime, PointF coordinates, float fontSize)
-    {
-        drawOn.DrawText(TimeUtils.FromUnixTimestamp((long)dateTime).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture), coordinates, fontSize);
-    }
-
-    public static void DrawTime(this Image drawOn, object dateTime, PointF coordinates, float fontSize)
-    {
-        drawOn.DrawText(TimeUtils.FromUnixTimestamp((long)dateTime).ToString("HH:mm:ss", CultureInfo.InvariantCulture), coordinates, fontSize);
-    }
-
-    public static void DrawPoolId(this Image drawOn, BigInteger poolId)
-    {
-        drawOn.DrawText($"#{poolId}", new PointF(981, 52), 24f);
-    }
-
     public static void DrawTokenBadge(this Image drawOn, string currencyName)
     {
         drawOn.DrawCurrencyBadge(currencyName, new PointF(554, 270), 16f);
