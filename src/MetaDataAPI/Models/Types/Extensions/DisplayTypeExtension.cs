a5a7edb3aef75426baa2c20fbc98458dc35c8133
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-10 00:40:42 +0300

    Remove unused classes

--- src/MetaDataAPI/Models/Types/Extensions/DisplayTypeExtension.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MetaDataAPI.Models.Types.Extensions;
-
-public static class DisplayTypeExtension
-{
-    public static string ToLowerString(this DisplayType displayType) =>
-        displayType.ToString().ToLower();
-}
