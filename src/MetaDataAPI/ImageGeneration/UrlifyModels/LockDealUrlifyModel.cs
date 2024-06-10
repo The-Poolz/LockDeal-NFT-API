32412b8c6835badf53022d77d890e2d99a1cd1a5
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-10 19:00:07 +0300

    - add `LockDealUrlifyModel` - add tests for `LockDealUrlifyModel`

---
+++ src/MetaDataAPI/ImageGeneration/UrlifyModels/LockDealUrlifyModel.cs
@@ -0,0 +1,16 @@
+﻿using MetaDataAPI.Models.Extension;
+using MetaDataAPI.Utils;
+using Net.Urlify.Attributes;
+
+namespace MetaDataAPI.ImageGeneration.UrlifyModels;
+
+public class LockDealUrlifyModel : BaseUrlifyModel
+{
+    [QueryStringProperty("Start Time")]
+    public string StartTime { get; set; }
+
+    public LockDealUrlifyModel(PoolInfo poolInfo) : base(poolInfo)
+    {
+        StartTime = $"{TimeUtils.FromUnixTimestamp((long)poolInfo.Params[1]):g}";
+    }
+}
--- tests/MetaDataAPI.Tests/ImageGeneration/UrlifyModels/BaseUrlifyModelTests.cs
+++ tests/MetaDataAPI.Tests/ImageGeneration/UrlifyModels/BaseUrlifyModelTests.cs
@@ -13,7 +13,7 @@ public class BaseUrlifyModelTests
     public class BuildUrl : SetEnvironments
     {
         [Fact]
-        internal void WhenExpectedUrlGenerated()
+        internal void BaseUrlifyModel_WhenExpectedUrlGenerated()
         {
             var poolInfo = new PoolInfo(new BasePoolInfo
             {
@@ -30,6 +30,26 @@ public class BaseUrlifyModelTests
             var result = nftHtml.BuildUrl();
 
             result.ToString().Should().Be("https://test.poolz.finance/nft.html?name=DealProvider&id=1234&tA=$TestTokenC|Left Amount|10000");
+        }
+
+        [Fact]
+        internal void LockDealUrlifyModel_WhenExpectedUrlGenerated()
+        {
+            var poolInfo = new PoolInfo(new BasePoolInfo
+            {
+                Name = "DealProvider",
+                PoolId = 1234,
+                Token = "0x32a23a97daEd1F41fA6dFE72Cc33aD5bCBdf17E1",
+                Owner = "0x000000000000000000000000000000000000dEaD",
+                VaultId = 2,
+                Provider = "0x70B0F2fd774376063faCC9178307cF1E18Ea3aF0",
+                Params = new List<BigInteger> { BigInteger.Parse("10000000000000000000000"), 1708089781 }
+            });
+            var nftHtml = new LockDealUrlifyModel(poolInfo);
+
+            var result = nftHtml.BuildUrl();
+
+            result.ToString().Should().Be("https://test.poolz.finance/nft.html?Start%20Time=2/16/2024 1:23 PM&name=DealProvider&id=1234&tA=$TestTokenC|Left Amount|10000");
         }
     }
 }
