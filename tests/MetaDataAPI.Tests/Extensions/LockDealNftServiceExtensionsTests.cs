080dabf7af055434b7e17fc5860332722b59e96f
ArdenHide <ardenhide@gmail.com>
2024-06-28 13:20:15 +0300

    - write `DateTimeExtensionsTests` - save not-working `LockDealNftServiceExtensionsTests`

---
+++ tests/MetaDataAPI.Tests/Extensions/LockDealNftServiceExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using Xunit;
+using System.Numerics;
+using FluentAssertions;
+using MetaDataAPI.Extensions;
+using Nethereum.RPC.Eth.DTOs;
+using poolz.finance.csharp.contracts.LockDealNFT;
+
+namespace MetaDataAPI.Tests.Extensions;
+
+public class LockDealNftServiceExtensionsTests
+{
+    public class IsPoolIdInSupplyRange
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        internal void ShouldReturnExpectedResult(BigInteger poolId, BigInteger totalSupply, bool expectedResult)
+        {
+            var lockDealNft = new Mock<ILockDealNFTService>();
+            lockDealNft
+                .Setup(x => x.TotalSupplyQueryAsync(It.IsAny<BlockParameter>()))
+                .ReturnsAsync(totalSupply);
+
+            var result = lockDealNft.Object.IsPoolIdInSupplyRange(poolId);
+
+            result.Should().Be(expectedResult);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { 5, 10, true };
+            yield return new object[] { 10, 10, false };
+            yield return new object[] { 15, 10, false };
+        }
+    }
+}
