a43ce79f991c3e238f7a1138b6dbb00b055a26c2
ArdenHide <ardenhide@gmail.com>
2024-06-28 14:00:54 +0300

    - tests for `FetchPoolInfo` methods from `LockDealNftServiceExtensions`

--- tests/MetaDataAPI.Tests/Extensions/LockDealNftServiceExtensionsTests.cs
+++ tests/MetaDataAPI.Tests/Extensions/LockDealNftServiceExtensionsTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using MetaDataAPI.Extensions;
 using Nethereum.RPC.Eth.DTOs;
 using poolz.finance.csharp.contracts.LockDealNFT;
+using poolz.finance.csharp.contracts.LockDealNFT.ContractDefinition;
 
 namespace MetaDataAPI.Tests.Extensions;
 
@@ -31,6 +32,55 @@ public class LockDealNftServiceExtensionsTests
             yield return new object[] { 5, 10, true };
             yield return new object[] { 10, 10, false };
             yield return new object[] { 15, 10, false };
+        }
+    }
+
+    public class FetchPoolInfo
+    {
+        private readonly Mock<ILockDealNFTService> _lockDealNft = new();
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        internal void FromLockDealNFTService_ShouldReturnExpectedPoolInfo(BigInteger poolId, GetFullDataOutputDTO fullData, BasePoolInfo[] expectedPoolInfo)
+        {
+            _lockDealNft
+                .Setup(x => x.GetFullDataQueryAsync(poolId, It.IsAny<BlockParameter>()))
+                .ReturnsAsync(fullData);
+
+            var result = _lockDealNft.Object.FetchPoolInfo(poolId);
+
+            result.Should().BeEquivalentTo(expectedPoolInfo);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        internal void FromServiceProvider_ShouldReturnExpectedPoolInfo(BigInteger poolId, GetFullDataOutputDTO fullData, BasePoolInfo[] expectedPoolInfo)
+        {
+            _lockDealNft
+                .Setup(x => x.GetFullDataQueryAsync(poolId, It.IsAny<BlockParameter>()))
+                .ReturnsAsync(fullData);
+
+            var serviceProvider = new Mock<IServiceProvider>();
+            serviceProvider
+                .Setup(x => x.GetService(typeof(ILockDealNFTService)))
+                .Returns(_lockDealNft.Object);
+
+            var result = serviceProvider.Object.FetchPoolInfo(poolId);
+
+            result.Should().BeEquivalentTo(expectedPoolInfo);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[]
+            {
+                new BigInteger(1),
+                new GetFullDataOutputDTO
+                {
+                    PoolInfo = new List<BasePoolInfo> { new() { PoolId = 1 } }
+                },
+                new BasePoolInfo[] { new() { PoolId = 1 } }
+            };
         }
     }
 }
