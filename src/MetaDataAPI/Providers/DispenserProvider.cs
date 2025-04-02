2d66810a3c209e1ea1d91241f9d21a0cb5f89c69
ArdenHide <ardenhide@gmail.com>
2025-04-02 13:28:45 +0300

    Implement `DispenserProvider`

---
+++ src/MetaDataAPI/Providers/DispenserProvider.cs
@@ -0,0 +1,10 @@
+﻿using MetaDataAPI.Services.ChainsInfo;
+using poolz.finance.csharp.contracts.LockDealNFT.ContractDefinition;
+
+namespace MetaDataAPI.Providers;
+
+public class DispenserProvider(BasePoolInfo[] poolsInfo, ChainInfo chainInfo, IServiceProvider serviceProvider)
+    : AbstractProvider(poolsInfo, chainInfo, serviceProvider)
+{
+    protected override string DescriptionTemplate => "DESCRIPTION_HERE, {{LeftAmount}}";
+}
