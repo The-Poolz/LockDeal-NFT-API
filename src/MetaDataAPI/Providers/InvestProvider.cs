6cb32ed4c403bb00d326f69af373d0398485209e
ArdenHide <ardenhide@gmail.com>
2025-08-04 13:20:00 +0300

    Add Invest Provider

---
+++ src/MetaDataAPI/Providers/InvestProvider.cs
@@ -0,0 +1,12 @@
+﻿using MetaDataAPI.Services.ChainsInfo;
+using poolz.finance.csharp.contracts.LockDealNFT.ContractDefinition;
+
+namespace MetaDataAPI.Providers;
+
+public class InvestProvider(BasePoolInfo[] poolsInfo, ChainInfo chainInfo, IServiceProvider serviceProvider)
+    : AbstractProvider(poolsInfo, chainInfo, serviceProvider)
+{
+    public string MaxInvestAmount { get; } = poolsInfo[0].Params[0].ToString();
+
+    protected override string DescriptionTemplate => "DESCRIPTION HERE";
+}
