e8524746cdf92a05e87e735d73477e3f191f1552
ArdenHide <ardenhide@gmail.com>
2025-08-04 16:58:25 +0300

    - update description

--- src/MetaDataAPI/Providers/InvestProvider.cs
+++ src/MetaDataAPI/Providers/InvestProvider.cs
@@ -1,12 +1,19 @@
-﻿using MetaDataAPI.Services.ChainsInfo;
+﻿using Nethereum.Util;
+using System.Globalization;
+using MetaDataAPI.Services.ChainsInfo;
 using poolz.finance.csharp.contracts.LockDealNFT.ContractDefinition;
 
 namespace MetaDataAPI.Providers;
 
-public class InvestProvider(BasePoolInfo[] poolsInfo, ChainInfo chainInfo, IServiceProvider serviceProvider)
-    : AbstractProvider(poolsInfo, chainInfo, serviceProvider)
+public class InvestProvider : AbstractProvider
 {
-    public string WeiMaxInvestAmount { get; } = poolsInfo[0].Params[0].ToString();
+    public string MaxInvestAmount { get; }
 
-    protected override string DescriptionTemplate => "DESCRIPTION HERE";
+    public InvestProvider(BasePoolInfo[] poolsInfo, ChainInfo chainInfo, IServiceProvider serviceProvider)
+        : base(poolsInfo, chainInfo, serviceProvider)
+    {
+        MaxInvestAmount = UnitConversion.Convert.FromWei(poolsInfo[0].Params[0], Erc20Token.Decimals).ToString(CultureInfo.InvariantCulture);
+    }
+
+    protected override string DescriptionTemplate => "This NFT enables the holder to invest up to {MaxInvestAmount} tokens of {Erc20Token.Symbol}.";
 }
