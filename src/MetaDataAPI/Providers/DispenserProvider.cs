2a8c293c0d5627d2eeb4294838f62bc790bbb861
ArdenHide <ardenhide@gmail.com>
2025-08-05 11:57:01 +0300

    Set N/A for LeftAmount in the DispenserProvider image

--- src/MetaDataAPI/Providers/DispenserProvider.cs
+++ src/MetaDataAPI/Providers/DispenserProvider.cs
@@ -6,5 +6,7 @@ namespace MetaDataAPI.Providers;
 public class DispenserProvider(BasePoolInfo[] poolsInfo, ChainInfo chainInfo, IServiceProvider serviceProvider)
     : AbstractProvider(poolsInfo, chainInfo, serviceProvider)
 {
+    public new string String_LeftAmount => "N/A";
+
     protected override string DescriptionTemplate => "This NFT allows the holder to dispense {{Erc20Token.Symbol}}.";
 }
