f9c280be320c0d6c4bfdaf63a6fb8fada201c6bb
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-10-16 12:34:48 +0300

    Update packages versions (#323)
    * - uninstall `FluentValidation` (package included in `Net.Cache.DynamoDb.ERC20`)
    - update `Net.Cache.DynamoDb.ERC20` version and update code
    
    * - update versions of `Amazon.Lambda.*` packages
    - remove `CovalentDb` package
    - update `Microsoft.Extensions.DependencyInjection` to latest version
    - update `Utils.EnvironmentManager` to latest version
    - update `PuppeteerSharp` to latest version
    
    * - fix

--- src/MetaDataAPI/Services/Erc20/Erc20Token.cs
+++ src/MetaDataAPI/Services/Erc20/Erc20Token.cs
@@ -1,30 +1,22 @@
-﻿using Net.Cache.DynamoDb.ERC20.Models;
-using Net.Web3.EthereumWallet.Extensions;
+﻿using Net.Web3.EthereumWallet.Extensions;
+using Net.Cache.DynamoDb.ERC20.DynamoDb.Models;
 
 namespace MetaDataAPI.Services.Erc20;
 
-public class Erc20Token
+public class Erc20Token(string name, string symbol, string address, byte decimals)
 {
-    public string Name { get; }
-    public string Symbol { get; }
-    public string Address { get; }
-    public byte Decimals { get; }
+    public string Name { get; } = name;
+    public string Symbol { get; } = symbol;
+    public string Address { get; } = address;
+    public byte Decimals { get; } = decimals;
 
-    public Erc20Token(string name, string symbol, string address, byte decimals)
-    {
-        Name = name;
-        Symbol = symbol;
-        Address = address;
-        Decimals = decimals;
-    }
-
-    public Erc20Token(ERC20DynamoDbTable cacheErc20)
-    {
-        Name = cacheErc20.Name;
-        Symbol = cacheErc20.Symbol;
-        Address = cacheErc20.Address;
-        Decimals = cacheErc20.Decimals;
-    }
+    public Erc20Token(Erc20TokenDynamoDbEntry cacheErc20) : this(
+        cacheErc20.Name,
+        cacheErc20.Symbol,
+        cacheErc20.Address,
+        cacheErc20.Decimals
+    )
+    { }
 
     public override string ToString() => $"{Name} ({Symbol}@{Address.ToShortAddress(5)})";
 }
