f8777f14ef73b167b68b0cd4210d69d2500c6b26
ArdenHide <ardenhide@gmail.com>
2025-10-20 18:07:56 +0300

    Implement `/favicon.ico`

---
+++ src/MetaDataAPI/Models/BinaryLambdaResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace MetaDataAPI.Models;
+
+public class FaviconLambdaResponse : LambdaResponse
+{
+    private const string FaviconPath = "/opt/favicon.ico";
+
+    public FaviconLambdaResponse() : this(File.ReadAllBytes(FaviconPath)) { }
+
+    public FaviconLambdaResponse(byte[] bytes) : base(Convert.ToBase64String(bytes), HttpStatusCode.OK)
+    {
+        IsBase64Encoded = true;
+        Headers = new Dictionary<string, string>
+        {
+            ["Content-Type"] = "image/x-icon"
+        };
+    }
+}
