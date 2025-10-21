b2aece95fa7034aae164c7426e4ecd1f8baa37ca
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-10-21 13:55:18 +0300

    Log if image generated or received by env variable (#342)
    * Log if image generated or received by env variable
    
    * - simplify if

--- src/MetaDataAPI/Services/Erc20/Erc20Provider.cs
+++ src/MetaDataAPI/Services/Erc20/Erc20Provider.cs
@@ -19,7 +19,7 @@ public class Erc20Provider(IErc20CacheService erc20Cache) : IErc20Provider
             .WithDynamoDBClient(() => new AmazonDynamoDBClient(
                 new AmazonDynamoDBConfig
                 {
-                    RegionEndpoint = RegionEndpoint.GetBySystemName(Environments.OVERRIDE_AWS_REGION.GetRequired())
+                    RegionEndpoint = RegionEndpoint.GetBySystemName(Env.OVERRIDE_AWS_REGION.GetRequired())
                 }
             ))
         ),
@@ -37,7 +37,7 @@ public class Erc20Provider(IErc20CacheService erc20Cache) : IErc20Provider
         var cache = erc20Cache.GetOrAddAsync(
             new HashKey(chainId, address),
             () => Task.FromResult(rpcUrl),
-            () => Task.FromResult(new EthereumAddress(Environments.MULTI_CALL_V3_ADDRESS.GetRequired()))
+            () => Task.FromResult(new EthereumAddress(Env.MULTI_CALL_V3_ADDRESS.GetRequired()))
         ).GetAwaiter().GetResult();
         return new Erc20Token(cache);
     }
