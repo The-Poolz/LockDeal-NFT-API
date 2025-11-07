6cceed0f65176e25c41542a85407b0979b01c66a
ArdenHide <ardenhide@gmail.com>
2025-11-07 15:51:49 +0300

    Log used RPC url when error occurred
    - create custom DelegatingHandler

---
+++ src/MetaDataAPI/Services/Http/FailureOnlyRpcLoggingHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Amazon.Lambda.Core;
+
+namespace MetaDataAPI.Services.Http;
+
+public class FailureOnlyRpcLoggingHandler(HttpMessageHandler inner, ILambdaLogger log) : DelegatingHandler(inner)
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage req, CancellationToken ct)
+    {
+        try
+        {
+            return await base.SendAsync(req, ct);
+        }
+        catch (HttpRequestException ex)
+        {
+            var logMessage = new StringBuilder()
+                .AppendLine($"RPC HTTP EXCEPTION {req.Method} {req.RequestUri}")
+                .AppendLine($"EXCEPTION: {ex}")
+                .ToString();
+            log.LogCritical(logMessage);
+            throw;
+        }
+    }
+}
