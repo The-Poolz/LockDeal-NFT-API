a65c5899332965062b305b0db043df7f7d45cd9f
ArdenHide <ardenhide@gmail.com>
2025-11-13 13:11:46 +0300

    - include retries

--- src/MetaDataAPI/Services/Erc20/Erc20Provider.cs
+++ src/MetaDataAPI/Services/Erc20/Erc20Provider.cs
@@ -4,11 +4,16 @@ using Net.Cache.DynamoDb.ERC20;
 using MetaDataAPI.Services.Http;
 using EnvironmentManager.Extensions;
 using MetaDataAPI.Services.ChainsInfo;
+using Poolz.Finance.CSharp.Polly.Extensions;
 using Net.Cache.DynamoDb.ERC20.DynamoDb.Models;
 
 namespace MetaDataAPI.Services.Erc20;
 
-public class Erc20Provider(IErc20CacheService erc20Cache, IWeb3Factory web3Factory) : IErc20Provider
+public class Erc20Provider(
+    IErc20CacheService erc20Cache,
+    IWeb3Factory web3Factory,
+    IRetryExecutor retry
+) : IErc20Provider
 {
     public Erc20Token GetErc20Token(ChainInfo chainInfo, EthereumAddress address)
     {
@@ -17,11 +22,14 @@ public class Erc20Provider(IErc20CacheService erc20Cache, IWeb3Factory web3Facto
 
     public Erc20Token GetErc20Token(string rpcUrl, long chainId, EthereumAddress address)
     {
-        var cache = erc20Cache.GetOrAddAsync(
-            new HashKey(chainId, address),
-            () => Task.FromResult(web3Factory.Create(rpcUrl)),
-            () => Task.FromResult(new EthereumAddress(Env.MULTI_CALL_V3_ADDRESS.GetRequired()))
-        ).GetAwaiter().GetResult();
+        var cache = retry.Execute(_ =>
+        {
+            return erc20Cache.GetOrAddAsync(
+                new HashKey(chainId, address),
+                () => Task.FromResult(web3Factory.Create(rpcUrl)),
+                () => Task.FromResult(new EthereumAddress(Env.MULTI_CALL_V3_ADDRESS.GetRequired()))
+            ).GetAwaiter().GetResult();
+        });
         return new Erc20Token(cache);
     }
 }
--- src/MetaDataAPI/Services/Http/HttpClientFactory.cs
+++ src/MetaDataAPI/Services/Http/HttpClientFactory.cs
@@ -12,7 +12,6 @@ public class HttpClientFactory : IHttpClientFactory
         };
 
         configureHeaders?.Invoke(client.DefaultRequestHeaders);
-
         return client;
     }
 }
